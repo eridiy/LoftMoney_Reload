9b0fdea773de475ff5cda9369cbe2160497ab3a8
EgorFedonin <fedonin69@gmail.com>
2022-02-09 18:06:25 +0300

    Initial commit

---
+++ app/src/main/java/com/eridiy/loftmoney_reload/ActivityExpenses.java
@@ -0,0 +1,14 @@
+package com.eridiy.loftmoney_reload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityExpenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses);
+    }
+}
